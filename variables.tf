8d242e3ad0843fe0f3dc6ee903601ab9153d4cda
rajjdevops <65726128+rajjdevops@users.noreply.github.com>
2021-07-08 09:35:32 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "access_key" {}
 
 variable "secret_key" {}
 
-variable "region" {
-  default = "us-east-2"
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "us-east-1"
 }
