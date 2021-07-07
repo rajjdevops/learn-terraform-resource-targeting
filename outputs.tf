19095022899e237c1eb47de02c3504948fea6617
rajjdevops <65726128+rajjdevops@users.noreply.github.com>
2021-07-08 09:12:56 +1000

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+# Output declarations
+
+output "bucket_name" {
+  description = "Randomly generated bucket name."
+ # value       = random_pet.bucket_name.id
+   value      = module.s3_bucket.s3_bucket_id
+}
+
+output "bucket_arn" {
+  description = "ARN of bucket"
+  value       = module.s3_bucket.s3_bucket_arn
+}
